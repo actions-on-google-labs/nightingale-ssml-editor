d14c4ca0f690c3602c23bcbccc1724b0a86ec4bb
Jacques David Commarmond <david.commarmond@gws-technologies.com>
2022-07-25 20:33:17 +0400

    Revert "Fixed Export SSML getting wrong begin values"
    This reverts commit 4bea9120481f444d8a6edc82151120e999f6ba8d.

--- src/modules/ssml-timeline.ts
+++ src/modules/ssml-timeline.ts
@@ -516,13 +516,16 @@ export class SsmlTimeline extends PolymerElement {
       const track = tracks[i];
       if (!track) continue;
       trackStarts[i] = track[0].time; // Get first block time as start
-      for (let j = 0; j < track.length; j++) {
+      for (let j = 0; j < track.length - 1; j++) {
         if (track[j].type == 'break') continue;
         // Clear any previous track data
-        tracks[i][j].begin = undefined
+        tracks[i][j+1].begin = undefined
+        const gapStart = track[j + 1].time;
         const block = track[j];
-        const begin = block.time
-        tracks[i][j].begin = begin
+        const begin = (gapStart - block.time - block.duration!)
+        if (block.time + block.duration! < gapStart) {
+          tracks[i][j+1].begin = begin
+        }
       }
     }
 
